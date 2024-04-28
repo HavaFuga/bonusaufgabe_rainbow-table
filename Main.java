684a1714de0ef6d058563e122f738f8fe952ac53
HavaFuga <hava.fuga@students.fhnw.ch>
2024-04-28 13:59:44 +0200

    feat: add find pw from hash algorithm

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        RainbowTable rt = new RainbowTable();
+        RainbowTable rt = new RainbowTable(2000, 2000, 7);
+        System.out.println("Table generated");
         rt.findPassword("1d56a37fb6b08aa709fe90e12ca59e12");
     }
 }
--- RainbowTable.java
+++ RainbowTable.java
@@ -1,59 +1,125 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class RainbowTable {
     private String Z = "0123456789abcdefghijklmnopqrstuvwxyz";
+    private int chainsLength;
+    private int chainsCount;
+    private int passwordsLength;
+    private Map<String, String> rainbowTable;
 
-    public RainbowTable() {
-        int chainsLength = 2000;
-        int passwordsLength = 7;
-        int chainsCount = 2000;
-
+    public RainbowTable(int cl, int cc, int pl) {
+        chainsLength = cl;
+        chainsCount = cc;
+        passwordsLength = pl;
 
         // generate rainbow table, should be 2000
-        Map<String, String> rainbowTable = generateRainbowTable(chainsLength, passwordsLength, chainsCount);
+        rainbowTable = generateRainbowTable();
+        writeHashMapToFile(rainbowTable);
 
-        // example
-        System.out.println("Beispiel: Die erste Kette:");
+        // Check for testing
+        /*System.out.println("Beispiel: Die erste Kette:");
         String firstpw = "0000000";
         String lastpw = rainbowTable.get(firstpw);
-        System.out.println(firstpw + " -> " + lastpw);
+        System.out.println(firstpw + " -> " + lastpw);*/
     }
 
-    public String findPassword(String s) {
+    private void writeHashMapToFile(Map<String, String> hashMap) {
+        String filePath = "output.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+                writer.write(entry.getKey() + ":" + entry.getValue());
+                writer.newLine();
+            }
+            System.out.println("HashMap successfully written to file: " + filePath);
+        } catch (IOException e) {
+            System.out.println("Error writing HashMap to file: " + e.getMessage());
+        }
+    }
 
+
+    public String findPassword(String hash) {
+        String firstpw = "";
+
+        outerloop:
+        for (int i = chainsCount - 1; i >= 0; i--) {
+            String currentHash = hash;
+            String pw = "";
+
+            // encript until R2000
+            for (int j = i; j < chainsLength; j++) {
+                pw = reduction(currentHash, passwordsLength, j);
+                currentHash = generateHash(pw);
+            }
+
+            String lastPw = reduction(currentHash, passwordsLength, chainsLength -1);
+
+            if (rainbowTable.containsValue(pw)) {
+                firstpw = getKey(pw);
+                break outerloop;
+            }
+        }
+
+        System.out.println("firstpw: " + firstpw);
+
+        String cpw = firstpw;
+        String ch = generateHash(cpw);
+        // hash + reduktionsfunktion 2000 mal
+        for (int j = 0; j < chainsLength; j++) {
+            cpw = reduction(ch, passwordsLength, j);
+            ch = generateHash(cpw);
+            if (ch.equals(hash)){
+                System.out.println("your pw is: " + cpw);
+                break;
+            }
+        }
+        return cpw;
+    }
+
+    // got this code from https://stackoverflow.com/questions/1383797/java-hashmap-how-to-get-key-from-value
+    // since its one to one. probably not the smartest tho
+    private String getKey(String s) {
+        for (Map.Entry<String, String> entry : rainbowTable.entrySet()) {
+            if (Objects.equals(s, entry.getValue())) {
+                return entry.getKey().toString();
+            }
+        }
         return "";
     }
 
-    // Funktion zur Generierung der Rainbow-Tabelle
-    private Map<String, String> generateRainbowTable(int chainsLength, int passwordsLength, int chainsCount) {
+    // generate rainbowtable
+    private Map<String, String> generateRainbowTable() {
         Map<String, String> rainbowTable = new HashMap<>();
 
         // 2000 pws mit 7 lenght
         String currentPassword;
         String currentHash;
 
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < chainsCount; i++) {
             currentPassword = generatePassword(i, passwordsLength);
             currentHash = generateHash(currentPassword);
             String firstPw = currentPassword;
 
-
             // hash + reduktionsfunktion 2000 mal
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < chainsLength; j++) {
                 currentPassword = reduction(currentHash, passwordsLength, j);
-                System.out.println("Current pw: " + currentPassword);
                 currentHash = generateHash(currentPassword);
-                System.out.println("Current Hash: " + currentHash);
+//                if (firstPw.equals("00000rs")){
+//                    System.out.println("level: " + j);
+//                    System.out.println("Current pw: " + currentPassword);
+//                    System.out.println("Current Hash: " + currentHash);
+//                }
             }
 
-            String lastPw = reduction(currentHash, passwordsLength, 1999);
-
-            // Speichere die letzte Reduktion der Kette in der Rainbow-Tabelle
-            rainbowTable.put(firstPw, lastPw);
+            // save first and last pw
+            rainbowTable.put(firstPw, currentPassword);
         }
 
         return rainbowTable;
@@ -88,7 +154,7 @@ public class RainbowTable {
         }
     }
 
-    // reduktionsfunktion (nicht funktional bruh....)
+    // reduktionsfunktion
     private String reduction(String hash, int length, int level) {
         StringBuilder result = new StringBuilder();
         BigInteger h = new BigInteger(hash, 16);
